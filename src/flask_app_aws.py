3258a8462fb81f21e1abe93dee9ed47b6568cc52
Paul Tanger <detroitstylz@gmail.com>
2020-09-01 17:16:02 -0600

    tweaks to webpages

--- src/flask_app_aws.py
+++ src/flask_app_aws.py
@@ -8,12 +8,15 @@ import s3fs
 app = Flask(__name__, root_path='../app_engine') # template_folder='../app_engine/templates'
 
 @app.route('/')
-
 def index():
-    return render_template('jumbotron.html')
+    return render_template('index.html')
+
+@app.route('/cover')
+def cover():
+    return render_template('cover.html')
 
 if __name__ == '__main__':
     # testing
-    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=33507, threaded=True, debug=True)
     # production:
-    app.run(host='0.0.0.0', port=33507, threaded=True)
+    #app.run(host='0.0.0.0', port=33507, threaded=True)
